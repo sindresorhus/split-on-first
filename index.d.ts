42c50316134c73ea1ffe30512ae3e2fd6405fce5
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2019-02-09 21:26:59 -0600

    Add typescript typings

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Split a string on the first occurrence of a given separator.
+ * @param string The string to split.
+ * @param separator The separator to split on.
+ */
+declare function splitOnFirst(string: string, separator: string): [string, string?];
+
+export = splitOnFirst;
