3ae8f7706aee3e7dfc2e5eec03c055441f51a87e
Sindre Sorhus <sindresorhus@gmail.com>
2019-02-14 14:34:29 +0700

    Update index.test-d.ts

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,9 +1,6 @@
-import { expectType } from "tsd-check";
-import splitOnFirst from ".";
+import {expectType} from 'tsd-check';
+import splitOnFirst from '.';
 
-const split = splitOnFirst("abcbc", "b");
-
+const split = splitOnFirst('abcbc', 'b');
 expectType<string>(split[0]);
-
 expectType<string | undefined>(split[1]);
-
