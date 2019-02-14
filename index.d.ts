caa1042464364c310e2faa53531563202b6a9aa6
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2019-02-14 00:25:07 +0000

    Format TSDoc

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,9 @@
 /**
  * Split a string on the first occurrence of a given separator.
- * @param string The string to split.
- * @param separator The separator to split on.
+ * 
+ * @param string - The string to split.
+ * @param separator - The separator to split on.
+ * @returns The string split on the separator, if possible.
  */
 declare function splitOnFirst(string: string, separator: string): [string, string?];
 
