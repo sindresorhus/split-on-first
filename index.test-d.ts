06d43f831160a824cf2ffa58a7e54af46148d47f
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2019-02-14 00:20:47 +0000

    Add typings test

---
+++ index.test-d.ts
@@ -0,0 +1,9 @@
+import { expectType } from "tsd-check";
+import splitOnFirst from ".";
+
+const split = splitOnFirst("abcbc", "b");
+
+expectType<string>(split[0]);
+
+expectType<string | undefined>(split[1]);
+
